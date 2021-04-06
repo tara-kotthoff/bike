d7b29917be6dfd6caa1ae4b27cd2bef3aec1710b
tarabyte84 <tekotthoff@me.com>
2021-04-06 17:22:18 -0500

    added to database, repo

--- src/main/java/com/globomatics/bike/models/data/BikeRepository.java
+++ src/main/java/com/globomatics/bike/models/data/BikeRepository.java
@@ -2,7 +2,6 @@ package com.globomatics.bike.models.data;
 
 import com.globomatics.bike.models.Bike;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface BikeRepository extends JpaRepository <Bike, Long>{
 
